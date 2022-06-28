04bd6f31e2281e9ea9bc7c6f13447caea23c1b6e
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2022-06-28 09:04:51 +0700

    adding app.js

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv');
+
+const culturalHeritageRoutes = require('./src/routes/culturalHeritageRoutes');
+const authRoutes = require('./src/routes/authRoutes');
+require('./src/db/mongoose');
+
+dotenv.config();
+const app = express();
+
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${new Date().getTime()}-${file.originalname}`);
+    }
+})
+
+const fileFilter = (req, file, cb) => {
+    const fileTypes = /jpeg|jpg|png/;
+    const mimetype = fileTypes.test(file.mimetype);
+    if (mimetype) {
+        cb(null, true);
+    }
+    cb(null, false);
+}
+
+app.use(cookieParser());
+app.use(cors({
+    credentials: true,
+    origin: ['https://sigayantara-api.herokuapp.com', 'http://localhost:3000']
+}));
+
+app.use(express.json());
+
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
+app.use('/v1/images', express.static(path.join(__dirname, 'images')));
+app.use('/v1/cultural-heritage', culturalHeritageRoutes);
+app.use('/v1/auth', authRoutes);
+
+app.use((error, req, res, next) => {
+    res.status(error.status).json({
+        message: error.message,
+        data: error.data,
+    });
+});
+
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,54 +1,6 @@
-const express = require('express');
-const multer = require('multer');
-const path = require('path');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const dotenv = require('dotenv');
+const app = require('./app');
+const CONFIG = require('./src/config/config');
 
-const culturalHeritageRoutes = require('./src/routes/culturalHeritageRoutes');
-const authRoutes = require('./src/routes/authRoutes');
-require('./src/db/mongoose');
-
-dotenv.config();
-const port = process.env.PORT || 5000;
-const app = express();
-
-const fileStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './images');
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${new Date().getTime()}-${file.originalname}`);
-    }
-})
-
-const fileFilter = (req, file, cb) => {
-    const fileTypes = /jpeg|jpg|png/;
-    const mimetype = fileTypes.test(file.mimetype);
-    if (mimetype) {
-        cb(null, true);
-    }
-    cb(null, false);
-}
-
-app.use(cookieParser());
-app.use(cors({
-    credentials: true,
-    origin: ['https://sigayantara-api.herokuapp.com', 'http://localhost:3000']
-}));
-
-app.use(express.json());
-
-app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
-app.use('/v1/images', express.static(path.join(__dirname, 'images')));
-app.use('/v1/cultural-heritage', culturalHeritageRoutes);
-app.use('/v1/auth', authRoutes);
-
-app.use((error, req, res, next) => {
-    res.status(error.status).json({
-        message: error.message,
-        data: error.data,
-    });
-});
+const port = process.env.PORT || CONFIG.PORT;
 
 app.listen(port, () => console.log(`Connection Success at http://localhost:${port}`));
